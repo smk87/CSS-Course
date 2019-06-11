c659b72744eb400fccd1457f9b510299b665669d
smk87 <30999133+smk87@users.noreply.github.com>
2019-06-11 13:05:37 +0600

    Animation added to some elements.

--- shared.js
+++ shared.js
@@ -4,6 +4,7 @@ let modalNoButton = document.querySelector(".modal__action--negative");
 let selectPlanButton = document.querySelectorAll(".plan button");
 let toggleButton = document.querySelector(".toggle-button");
 let mobileNav = document.querySelector(".mobile-nav");
+let ctaButton = document.querySelector(".main-nav__item--cta");
 
 // console.dir(backdrop);
 for (let i = 0; i < selectPlanButton.length; i++) {
@@ -12,7 +13,10 @@ for (let i = 0; i < selectPlanButton.length; i++) {
     // backdrop.style.display = "block";
     // modal.className = "open"; // This will overwrite previous classes
     modal.classList.add("open");
-    backdrop.classList.add("open");
+    setTimeout(function() {
+      backdrop.classList.add("open");
+    }, 10);
+    backdrop.style.display = "block";
   });
 }
 
@@ -33,11 +37,30 @@ function closeModal() {
     modal.classList.remove("open");
   }
   backdrop.classList.remove("open");
+
+  setTimeout(function() {
+    backdrop.style.display = "none";
+  }, 200);
 }
 
 toggleButton.addEventListener("click", () => {
   // mobileNav.style.display = "block";
   // backdrop.style.display = "block";
   mobileNav.classList.add("open");
-  backdrop.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(function() {
+    backdrop.classList.add("open");
+  }, 10);
+});
+
+ctaButton.addEventListener("animationstart", event => {
+  console.log("Animation started.", event);
+});
+
+ctaButton.addEventListener("animationend", event => {
+  console.log("Animation ended.", event);
+});
+
+ctaButton.addEventListener("animationiteration", event => {
+  console.log("Animation iterated.", event);
 });
